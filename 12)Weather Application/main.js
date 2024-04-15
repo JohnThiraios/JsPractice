907d570567ce292cab2134b4f4018d83b8a259a6
John Thiraios <thiraios.john@gmail.com>
2024-04-15 09:50:51 +0300

    code update

--- 12)Weather Application/main.js
+++ 12)Weather Application/main.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "e8646496eae51d0966fee7150b460864";
+const apiKey = "";
 
 weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
